087c54def6df2531fd12d897940a235c16409625
Nathan Lafferty <lcgamer333@gmail.com>
2016-03-08 23:33:14 -0600

    initial git repo

---
+++ src/nodeserver.js
@@ -0,0 +1,20 @@
+/// <reference path="../typings/main.d.ts" />
+
+const port = 13082;
+
+var express = require('express');
+var jade = require('jade');
+var app = express();
+
+app.set('view engine', 'jade')
+
+app.get('/', function(req, res) {
+    res.render('index', {title: 'Portal0', message: 'Hello World'});
+});
+
+app.listen(port, function() {
+    console.log('Webserver started');
+});
+
+app.use('/static', express.static(__dirname + '/Client/Content'));
+app.set('views', __dirname + '/Client/Views');
