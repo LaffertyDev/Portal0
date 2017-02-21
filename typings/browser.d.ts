262ac36689b6375b7e5eb7da8dfa76841dda16b2
Nathan Lafferty <lcgamer333@gmail.com>
2017-02-20 19:08:54 -0600

    Added gitignore file

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/angular/index.d.ts" />
-/// <reference path="browser/ambient/es6-shim/index.d.ts" />
-/// <reference path="browser/ambient/express/index.d.ts" />
-/// <reference path="browser/ambient/jasmine/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
