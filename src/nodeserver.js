ac6fca82dfd0ec9d899f02ddd68f07f4313c08a4
Ha1fDead <lcgamer333@gmail.com>
2017-03-11 12:27:41 -0600

    Added 404 page

--- src/nodeserver.js
+++ src/nodeserver.js
@@ -44,6 +44,26 @@ app.get('/worldbuilder', function(req, res) {
     console.log('Worldbuilder Hit');
 });
 
+//taken from http://stackoverflow.com/a/9802006/2383477
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
 app.listen(port, function() {
     console.log('Webserver started');
 });
