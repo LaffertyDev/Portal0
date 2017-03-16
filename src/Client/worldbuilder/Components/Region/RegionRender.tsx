137015e389b18f48dec2f1e69f462b7cd0560056
Ha1fDead <lcgamer333@gmail.com>
2017-03-15 19:36:41 -0500

    improved region rendering

--- src/Client/worldbuilder/Components/Region/RegionRender.tsx
+++ src/Client/worldbuilder/Components/Region/RegionRender.tsx
@@ -13,27 +13,60 @@ export default class RegionRender extends React.Component<RegionRenderProps, voi
 
     render() {
         return (
-            <div>
+            <div className="laff-wb-region">
                 <div>
                     <h3>Region Info</h3>
-                    <p>Age (Years): {this.props.region.Age.toString()}</p>
-                    <p>Area (Acres): {this.props.region.AreaAcres.toString()}</p>
-                    <p>Population: {this.props.region.Population.toString()}</p>
-                    <p>LiveStock: {this.props.region.TotalLivestock.toString()}</p>
-                    <p>Fowl: {this.props.region.Fowl.toString()}</p>
-                    <p>Cows, Sheep, &amp; Pigs: {this.props.region.BurdenBeasts.toString()}</p>
+                    <p>
+                        <span>
+                            Age (Years): {this.props.region.Age.toString()}
+                        </span>
+                        <span>
+                            Area (Acres): {this.props.region.AreaAcres.toString()}
+                        </span>
+                        <span>
+                            Population: {this.props.region.Population.toString()}
+                        </span>
+                    </p>
+                    <h3>Livestock</h3>
+                    <p>
+                        <span>
+                            Total: {this.props.region.TotalLivestock.toString()}
+                        </span>
+                        <span>
+                            Fowl: {this.props.region.Fowl.toString()}
+                        </span>
+                        <span>
+                            Cows, Sheep, &amp; Pigs: {this.props.region.BurdenBeasts.toString()}
+                        </span>
+                    </p>
                 </div>
                 <div>
                     <h3>Castle Info</h3>
-                    <p>Total Castles: {this.props.region.TotalCastles.toString()}</p>
-                    <p>Active Castles: {this.props.region.ActiveCastles.toString()}</p>
-                    <p>Ruined Castles: {this.props.region.RuinedCastles.toString()}</p>
+                    <p>
+                        <span>
+                            Total Castles: {this.props.region.TotalCastles.toString()}
+                        </span>
+                        <span>
+                            Active Castles: {this.props.region.ActiveCastles.toString()}
+                        </span>
+                        <span>
+                            Ruined Castles: {this.props.region.RuinedCastles.toString()}
+                        </span>
+                    </p>
                 </div>
                 <div>
                     <h3>Settlement Info</h3>
-                    <p>#Cities: {this.props.region.Cities.length.toString()}</p>
-                    <p>#Towns: {this.props.region.Towns.length.toString()}</p>
-                    <p>#Villages: {this.props.region.Villages.length.toString()}</p>
+                    <p>
+                        <span>
+                            #Cities: {this.props.region.Cities.length.toString()}
+                        </span>
+                        <span>
+                            #Towns: {this.props.region.Towns.length.toString()}
+                        </span>
+                        <span>
+                            #Villages: {this.props.region.Villages.length.toString()}
+                        </span>
+                    </p>
                 </div>
             </div>
         );
