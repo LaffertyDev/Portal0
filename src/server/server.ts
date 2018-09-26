27d55a521470cf6f98063db4203695619543f4c5
Nathan Lafferty <lcgamer333@gmail.com>
2018-09-26 15:10:22 -0500

    merged and setup handlebars code

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as fs from "fs";
+import * as hbs from "handlebars";
 import * as http from "http";
 import * as https from "https";
 import * as Path from "path";
@@ -28,6 +29,71 @@ export class HttpServer {
 		const server = express();
 		server.use(express.static(PUBLIC_DIRECTORY_FULL_PATH));
 
+		server.set("view engine", "hbs");
+		server.set("views", Path.join(__dirname, PUBLIC_DIRECTORY, "views"));
+		server.set("view options", { layout: "_layout" });
+
+		// server.use(sass_middleware({
+		// 	src: __dirname + '/sass',
+		// 	dest: __dirname + '/client/content/css',
+		// 	prefix: '/css',
+		// 	debug: true
+		// }));
+
+		hbs.registerPartial("footer", fs.readFileSync(Path.join(__dirname, PUBLIC_DIRECTORY, "views/footer.hbs"), "utf8"));
+		hbs.registerPartial("header", fs.readFileSync(Path.join(__dirname, PUBLIC_DIRECTORY, "views/footer.hbs"), "utf8"));
+
+		server.get("/", (req, res) => {
+			res.render("index");
+			console.log("Index Hit");
+		});
+
+		server.get("/resume", (req, res) => {
+			res.render("resume");
+			console.log("Resume Hit");
+		});
+
+		server.get("/components", (req, res) => {
+			res.render("components");
+			console.log("Components Hit");
+		});
+
+		server.get("/worldbuilder", (req, res) => {
+			res.render("dnd/worldbuilder");
+			console.log("Worldbuilder Hit");
+		});
+		server.get("/dnd/action", (req, res) => {
+			res.render("dnd/dndaction");
+			console.log("action econ Hit");
+		});
+		server.get("/procgen", (req, res) => {
+			res.render("procgen/procgen");
+			console.log("proc gen Hit");
+		});
+		server.get("/dnd/battlemap", (req, res) => {
+			res.render("dnd/battlemap");
+			console.log("battlemap Hit");
+		});
+
+		// taken from http://stackoverflow.com/a/9802006/2383477
+		server.use((req, res, next) => {
+			res.status(404);
+			// respond with html page
+			if (req.accepts("html")) {
+				res.render("404", { url: req.url });
+				return;
+			}
+
+			// respond with json
+			if (req.accepts("json")) {
+				res.send({ error: "Not found" });
+				return;
+			}
+
+			// default to plain-text. send()
+			res.type("txt").send("Not found");
+		});
+
 		if (serveSource) {
 			/**
 			 * This handles sourcemaps. We only want to allow this on developer environments
@@ -46,90 +112,3 @@ export class HttpServer {
 		this.httpsServer.listen(SERVER_SECURE_PORT);
 	}
 }
-
-/*
-const port = 13082;
-
-//dependencies
-var express = require('express'),
-    app = express(),
-    sass = require('node-sass'),
-    path = require('path'),
-    sass_middleware = require('node-sass-middleware'),
-    hbs = require('hbs');
-//end dependencies
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/client/views');
-app.set('view options', { layout: '_layout' });
-
-//hbs.registerPartial('partial', fs.readFileSync(__dirname + '/Client/Views/partial.hbs', 'utf8'));
-hbs.registerPartials(__dirname + '/client/views/partials');
-
-//for some reason, we can't register the SASS directly under content (because it's included in the app.use)
-app.use(sass_middleware({
-    src: __dirname + '/sass',
-    dest: __dirname + '/client/content/css',
-    prefix: '/css',
-    debug: true
-}));
-
-app.use(express.static(__dirname + '/client/content'));
-app.use('/app', express.static(__dirname + '/client/app'));
-
-app.get('/', function(req, res) {
-    res.render('index');
-    console.log('Index Hit');
-});
-
-app.get('/resume', function(req, res) {
-    res.render('resume');
-    console.log('Resume Hit');
-});
-
-app.get('/components', function(req, res) {
-    res.render('components');
-    console.log('Components Hit');
-});
-
-app.get('/worldbuilder', function(req, res) {
-    res.render('dnd/worldbuilder');
-    console.log('Worldbuilder Hit');
-});
-app.get('/dnd/action', function(req, res) {
-    res.render('dnd/dndaction');
-    console.log('action econ Hit');
-});
-app.get('/procgen', function(req, res) {
-    res.render('procgen/procgen');
-    console.log('proc gen Hit');
-});
-app.get('/dnd/battlemap', function(req, res) {
-    res.render('dnd/battlemap');
-    console.log('battlemap Hit');
-});
-
-//taken from http://stackoverflow.com/a/9802006/2383477
-app.use(function(req, res, next){
-  res.status(404);
-
-  // respond with html page
-  if (req.accepts('html')) {
-    res.render('404', { url: req.url });
-    return;
-  }
-
-  // respond with json
-  if (req.accepts('json')) {
-    res.send({ error: 'Not found' });
-    return;
-  }
-
-  // default to plain-text. send()
-  res.type('txt').send('Not found');
-});
-
-app.listen(port, function() {
-    console.log('Webserver started');
-});
-*/
